c3f830386a24897c39b8b19a50f05066a8d8828b
joelgschwind <joel.gsch@gmail.com>
2019-03-09 18:40:39 +0100

    bliblablub

--- UI/ui.js
+++ UI/ui.js
@@ -1,8 +1,13 @@
-var startLocation;
 var mapProp;
 var map;
+
+var startLocation;
+var crosswalkLocation;
 var startLocationSet = false;
-var resultActive = false;
+var crosswalkLocationSet = false;
+
+var directionsService;
+var directionsDisplay;
 
 function myMap() {
     mapProp= {
@@ -13,6 +18,12 @@ function myMap() {
         tilt: 0,
     };
     map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+    directionsService = new google.maps.DirectionsService,
+    directionsDisplay = new google.maps.DirectionsRenderer({
+      map: map
+    }),
+
 
     google.maps.event.addListener(map, 'click', function(event) {
         placeMarker(event.latLng);
@@ -36,7 +47,9 @@ function placeMarker(latLng) {
 }
 
 function placeMarkerCrosswalk(lat, lng) {
-    console.log(lat + ' ' + lng);
+    if (crosswalkLocationSet) {
+        crosswalkLocation.setMap(null);
+    }
     var marker = new google.maps.Marker({
         position: {lat: lat, lng: lng},
         map: map,
@@ -45,9 +58,15 @@ function placeMarkerCrosswalk(lat, lng) {
             url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
         }
     });
+    crosswalkLocationSet = true;
+    crosswalkLocation = marker;
 }
 
 function process() {
+    getClassification(startLocation.getPosition().lat(), startLocation.getPosition().lng())
+}
+
+function setImage() {
 
 }
 
@@ -58,8 +77,8 @@ var apiIP = 'http://127.0.0.1:5000/';
  * Returns a "Result" object.
  */
 function getClassification(lat, lng) {
-    const httpRequest = new XMLHttpRequest();
-    const formData = new FormData();
+    var httpRequest = new XMLHttpRequest();
+    var formData = new FormData();
 
     formData.append('lat', lat);
     formData.append('lng', lng);
@@ -68,17 +87,13 @@ function getClassification(lat, lng) {
 
     httpRequest.onreadystatechange = () => {
         if (httpRequest.readyState === 4) {
-        const response = httpRequest.response;
-        const resultObj = JSON.parse(response);
-
-        /*
-        for (const i of resultObj.segments) {
-            console.log(i.start);
-            for (const j of i.gestures) {
-            console.log(j.name);
-            }
-        }
-        */
+            var response = httpRequest.response;
+            var slice = response.indexOf('/');
+            var lat = response.substring(0,slice);
+            var lng = response.substring(slice + 1, response.length);
+            console.log(lat + ' ' + lng);
+            placeMarkerCrosswalk(parseFloat(lat), parseFloat(lng));
+            document.getElementById("classificationImg").src = "../API/classification.png";
         }
     };
 }
